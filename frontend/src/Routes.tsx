faf4866516df3a02146ac0fb198a7790443f9b5b
Tiago Almeida <tiagodalmeida87@hotmail.com>
2021-12-25 18:52:33 -0300

    update frontend

---
+++ frontend/src/Routes.tsx
@@ -0,0 +1,12 @@
+import { BrowserRouter } from 'react-router-dom';
+
+function Routes = () => {
+    return (
+        <BrowserRouter>
+            
+
+        </BrowserRouter>
+    );
+}
+
+export default Routes;
